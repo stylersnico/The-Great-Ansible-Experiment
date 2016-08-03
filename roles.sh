29b134c1944b4bd3cd89394f69d6d301914a8549
Nicolas Simond <nicolassimond@posteo.ch>
2016-08-03 16:33:21 +0200

    Add files via upload

---
+++ roles.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ansible-galaxy install bennojoy.nginx uoi-io.galera weareinteractive.unison geerlingguy.security franklinkim.ufw resmo.ntp SimpliField.haproxy geerlingguy.munin geerlingguy.munin-node
